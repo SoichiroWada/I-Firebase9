827c517bac82dbd70437b5021b173ddbba5de0ef
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-28 01:31:33 +0900

    5th commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import { initializeApp} from 'firebase/app'
+import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDd-SnaRR15Id3pKDE4TeeV7lT4H7QcTVo",
-  authDomain: "fir-dojo-232a1.firebaseapp.com",
-  projectId: "fir-dojo-232a1",
-  storageBucket: "fir-dojo-232a1.firebasestorage.app",
-  messagingSenderId: "108155588492",
-  appId: "1:108155588492:web:dfdd41d013c8da0fe110a9",
-  measurementId: "G-RWNNWMXYKM"
+    apiKey: "AIzaSyDd-SnaRR15Id3pKDE4TeeV7lT4H7QcTVo",
+    authDomain: "fir-dojo-232a1.firebaseapp.com",
+    projectId: "fir-dojo-232a1",
+    storageBucket: "fir-dojo-232a1.firebasestorage.app",
+    messagingSenderId: "108155588492",
+    appId: "1:108155588492:web:dfdd41d013c8da0fe110a9",
+    measurementId: "G-RWNNWMXYKM"
 };
 
 //init firebase app
@@ -23,6 +23,12 @@ const colRef = collection(db, 'books')
 
 //get collection data
 getDocs(colRef)
-.then((snapshot) => {
-    console.log(snapshot.docs)
-})
+    .then((snapshot) => {
+        console.log(snapshot.docs)
+        let books = [];
+        snapshot.docs.forEach((doc) => {
+            console.log(doc);
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    })
