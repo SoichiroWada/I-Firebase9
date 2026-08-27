7885d0e4e1fe497a90fa8887e328f3f5a8cfe3a7
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-28 00:56:48 +0900

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello from index.js')
+import { initializeApp} from 'firebase/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDd-SnaRR15Id3pKDE4TeeV7lT4H7QcTVo",
+  authDomain: "fir-dojo-232a1.firebaseapp.com",
+  projectId: "fir-dojo-232a1",
+  storageBucket: "fir-dojo-232a1.firebasestorage.app",
+  messagingSenderId: "108155588492",
+  appId: "1:108155588492:web:dfdd41d013c8da0fe110a9",
+  measurementId: "G-RWNNWMXYKM"
+};
+
+initializeApp(firebaseConfig);
