d8d2ff94bd47ab4340d407e84336408bbfdf145e
Soichiro Wada <soichirowada@ipmulticast.net>
2026-09-01 00:32:17 +0900

    Add Firebase authentication: signup, login, and logout functionality
    - Implemented user signup with email and password
    - Added user login functionality
    - Included logout button to sign out users
    - Updated index.html to include forms for signup and login

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import {
     onSnapshot, query, where, orderBy, serverTimestamp, getDoc,
     updateDoc
 } from 'firebase/firestore'
-import firebaseConfig from './firebaseConfig';
+import firebaseConfig from './firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword,
+    signOut, signInWithEmailAndPassword } from 'firebase/auth'
 
 //init firebase app
 initializeApp(firebaseConfig);
 
 //init services
 const db = getFirestore();
+const auth = getAuth();
 
 //collection ref
 const colRef = collection(db, 'books');
@@ -106,4 +109,51 @@ updateForm.addEventListener('submit', (e) => {
         .then((doc) => {
             console.log(doc.data(), doc.id)
         })
+})
+
+// signing users up
+const signupForm = document.querySelector('.signup');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = signupForm.email.value
+    const password = signupForm.password.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(cred => {
+            console.log('user created:', cred.user)
+            signupForm.reset()
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+})
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 })
